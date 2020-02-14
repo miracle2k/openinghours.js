ab2c046506e346c26538eb1fee2f5c079b2393b9
Michael Elsdörfer <michael@elsdoerfer.info>
2020-02-14 01:38:24 +0000

    Generate an iife bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,48 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json'
 
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    typescript({
-      typescript: require('typescript'),
-      objectHashIgnoreUnknownHack: true,
-    }),
-    // nodeResolve({
-    //   jsnext: true
-    // }),
-    visualizer()
-  ],
-}
+const typescriptConfig = typescript({
+  typescript: require('typescript'),
+  objectHashIgnoreUnknownHack: true,
+});
+
+
+const plugins = [
+  typescriptConfig,
+  visualizer()
+];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      }
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins,
+  },
+  {
+    input: 'src/index.ts',
+    output: [      
+      {
+        file: 'dist/index.iife.standalone.js',
+        format: 'iife',
+        name: 'openinghours'
+      },
+    ],
+    plugins: [
+      typescriptConfig,
+      nodeResolve()
+    ],
+  }
+]
